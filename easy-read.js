eef45a790fd08d6e87dd1c75eda41695e9e11711
adkwalters <adkwalters@gmail.com>
2021-07-06 17:24:18 +0100

    Initial commit for project Easy Read

---
+++ easy-read.js
@@ -0,0 +1,9 @@
+function showNav() {
+    navList = document.getElementById('header-nav-list');
+    if (navList.style.display === "none") {
+        navList.style.display = "block";
+    } else {
+        navList.style.display = "none";
+    }
+}
+
