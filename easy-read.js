3c9b7fac31dba5314eea43fe816674b1c25a483f
adkwalters <adkwalters@gmail.com>
2021-07-08 16:32:10 +0100

    dropdown menu bug fixed - no need to click twice

--- easy-read.js
+++ easy-read.js
@@ -1,9 +1,9 @@
 function showNav() {
     navList = document.getElementById('header-nav-list');
-    if (navList.style.display === "none") {
-        navList.style.display = "block";
-    } else {
+    if (navList.style.display === "block") {
         navList.style.display = "none";
+    } else {
+        navList.style.display = "block";
     }
 }
 
