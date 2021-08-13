da0d4eaa06569385c9d24b59a1171f8c91235c94
adkwalters <adkwalters@gmail.com>
2021-08-13 23:45:46 +0100

    improve backend data gathering
    - get article, source, paragraph, and summary data
    - save persistently to database

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+# $env:FLASK_APP = "application.py"
+# set FLASK_APP=application.py
+
 # Import libraries
 from flask import Flask, redirect, render_template, request
 import sqlite3
@@ -9,7 +12,7 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Connect SQLite database
-connection = sqlite3.connect("easyRead.db")
+connection = sqlite3.connect("easy_read.db", check_same_thread=False) # allows returned connection to be shared between multiple threads
 cursor = connection.cursor()
 
 # Test index page
@@ -23,27 +26,49 @@ def index():
 def createArticle():
 
     if request.method == "POST":
-        
-        source_website = request.form.get("source-website")
-        source_author = request.form.get("source-author")
-        source_title = request.form.get("source-title")
-        source_hyperlink = request.form.get("source-hyperlink")
-        article_title = request.form.get("article-title")
-        article_date_published = request.form.get("article-date-pblished")
-        article_date_updated = request.form.get("article-date-updated")
-        article_categories = request.form.getlist("article-categories")
-        article_summaries = request.form.getlist("article-summaries[]") 
 
-        print(article_summaries)
-        print(request.form.has_key(article_summaries))
-        f = request.form
-        for key in f.keys():
-            for value in f.getlist(key):
-                print(key,":",value)
+        # Get static form content
+        article_title = request.form.get("article-form-title")
+        article_description = request.form.get("article-form-description")
+        article_date_published = request.form.get("article-form-date-published")
+        article_date_updated = request.form.get("article-form-date-updated")
+        source_website = request.form.get("article-form-source-website")
+        source_author = request.form.get("article-form-source-author")
+        source_title = request.form.get("article-form-source-title")
+        source_hyperlink = request.form.get("article-form-source-hyperlink")
 
-        # cursor.execute()
-        # Write data to db from form
+        # Insert article content into database
+        article_insert = cursor.execute("INSERT INTO articles (article_title, article_description, date_published, date_updated) VALUES (?, ?, ?, ?)", 
+                        (article_title, article_description, article_date_published, article_date_updated))
+
+        # Get article ID primary key to use as a foreign key in other tables
+        article_id = article_insert.lastrowid
+       
+        # Insert source content into database
+        source_insert = cursor.execute("INSERT INTO sources (article_id, website, source_author, source_title, hyperlink) VALUES (?, ?, ?, ?, ?)", 
+                        (article_id, source_website, source_author, source_title, source_hyperlink))
         
+        # Get paragraph content into database
+        for key in request.form.keys():
+            if "paragraph" in key:
+                paragraphs_insert = cursor.execute("INSERT INTO paragraphs (article_id) VALUES (?)", (article_id,))
+
+                paragraph_id = paragraphs_insert.lastrowid
+        
+                cursor.execute("INSERT INTO summaries (paragraphs_id, level, summary_id_name, content) VALUES (?, ?, ?, ?)",
+                    (paragraph_id, key[-1], key, request.form[key]))
+
+        #  # Get paragraph content into database
+        # for key in request.form.keys():
+        #     if "paragraph" in key:
+        #         cursor.execute("INSERT INTO paragraphs (article_id, summary_id_name, content) VALUES (?, ?, ?)",
+        #         (article_id, key, request.form[key]))
+        #         # print(key, request.form[key])
+
+
+        connection.commit()
+        # connection.close() Not needed?
+
         return redirect("/")
     
     else:
