694acc01a34e660bfd4416da83e3aba670487d4a
adkwalters <adkwalters@gmail.com>
2021-08-15 10:27:41 +0100

    improve cms frontend - user can add delete category elements to form
    - wanrning, categories not yet connected to backend

--- static/easy-read-cms.js
+++ static/easy-read-cms.js
@@ -1,14 +1,47 @@
 // // || Article CMS 
+    
 
 // || Event Listeners 
 
     // Add event listeners once DOM is loaded
     function listenForContentLoaded() {
 
+        // Add selected category to article form
+        const addCategoryButton = document.getElementById("article-form-add-category-button");
+        const articleFormCategoriesList = document.querySelector(".article-form-categories-list")
+
+        // Upon click, add an element to the category list
+        addCategoryButton.addEventListener("click", e => {
+            const newCategory = document.createElement("li");
+            newCategory.setAttribute("class", "label-size label-colour");
+            
+            // Take the selected value and display it in the element
+            const categoryText = document.querySelector("input[name='article-form-categories']").value
+
+            if (categoryText) {
+                newCategory.textContent = categoryText;
+                articleFormCategoriesList.appendChild(newCategory);
+            }
+
+            // How to avoid adding a category twice?
+            // How to get the data of all categories added?
+        })
+
+        // Delete category from article form
+        const delCategoryButton = document.getElementById("article-form-del-category-button");
+
+        // Upon click, if the list has an item, delete the ultimate item (LIFO)
+        delCategoryButton.addEventListener("click", () => {
+            if (articleFormCategoriesList.firstChild) {
+                articleFormCategoriesList.lastChild.remove();
+            }
+        })
+
         // Add paragraph to content section of article form
         const addParagraphButton = document.getElementById("add-paragraph");
         const articleContent = document.getElementById("article-form-content");
         
+        // Upon click, add a custom element with an index equal to the number of paragraphs displayed 
         addParagraphButton.addEventListener("click", () => {
             const newParagraph = document.createElement("create-paragraph");
             let paragraphIndex = document.getElementsByTagName("create-paragraph").length; 
