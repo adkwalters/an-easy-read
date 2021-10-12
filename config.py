7cd38182dbbc1c608a7d2910a99456f97983f85f
adkwalters <adkwalters@gmail.com>
2021-10-12 15:21:50 +0100

    Test committing part of a file using patch mode
    - commit only the comments to the cofig file
        - git add -p config.py
            - s: split hunks
            - e: edit hunks
                - replace + with # to remove additions

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 
 # Class-based app configuration
 class Config(object):
+
+    # Set secret key !! to change
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a fallback'
+    
+    # Auto-reload templates upon change
     TEMPLATES_AUTO_RELOAD = True
