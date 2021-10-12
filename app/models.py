2087e0a585cd7791bcdf5e6516f57e5742b05d94
adkwalters <adkwalters@gmail.com>
2021-10-12 22:06:11 +0100

    pass test: add user by username and email

--- app/models.py
+++ app/models.py
@@ -2,3 +2,5 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
