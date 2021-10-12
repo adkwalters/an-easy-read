3e7916753a3a4886f828e284ac896dd42a59c7c7
adkwalters <adkwalters@gmail.com>
2021-10-12 19:30:25 +0100

    Add db object to represent database

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,7 @@ app = Flask(__name__)
 # Configure app from config class
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+
 # import here at bottom as workaround for circular import
 from app import routes
