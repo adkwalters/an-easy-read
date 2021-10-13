97eee9379597328e8401b4f86f44eb3326f6c3f5
adkwalters <adkwalters@gmail.com>
2021-10-13 19:47:49 +0100

    Improve test: log user in and out
    - move test assertions and logout call into text_request_context
        - access to request object limited to block scope
    - change logout assertion to test is_authenticated
        - anonymous object does not trigger False

--- tests.py
+++ tests.py
@@ -19,14 +19,14 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
         users = User.query.all()
 
-        self.assertTrue(len(users) > 0)    # User in database
+        self.assertTrue(len(users) > 0)
 
     def test_hash_user_password(self):
         u = User(username='Andrew', email='andrew@email.com')
         u.set_password('password')
 
-        self.assertTrue(u.check_password('password'))    # Correct password
-        self.assertFalse(u.check_password('not the password'))    # Incorrect password
+        self.assertTrue(u.check_password('password'))
+        self.assertFalse(u.check_password('not the password'))
 
     def test_log_user_in_and_out(self):
         u = User(username='Andrew', email='andrew@email.com')
@@ -35,17 +35,16 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
         user = User.query.filter_by(username='Andrew').first()
 
-        self.assertFalse(current_user)    # No user logged in
+        self.assertFalse(current_user)
 
         with app.test_request_context():
             login_user(user)
 
-        self.assertTrue(current_user)    # User logged in
+            self.assertTrue(current_user)
 
-        with app.test_request_context():
-            logout_user(user)
+            logout_user()
 
-        self.assertFalse(current_user)    # No user logged in
+            self.assertFalse(current_user.is_authenticated)
 
 
 if __name__ == '__main__':
