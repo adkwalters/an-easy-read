ef720e6ffbd2c10542d35912a1bd5d333e5cf422
adkwalters <adkwalters@gmail.com>
2021-10-13 09:12:54 +0100

    make test code more readable
    - move inline comments next to code to reduce lines
    - remove non-self-documenting comments
        - assume reader's skill level is higher
    - align function names with convention

--- tests.py
+++ tests.py
@@ -4,29 +4,18 @@ from app.models import User
 
 
 class UserModelCase(unittest.TestCase):
-    def setUp(self):
-        
-        # Use in-memory SQLite database
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-        
-        # Create tables
+    def set_up(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'    # Use in-memory SQLite database
         db.create_all()
 
-    def tearDown(self):
-
-        # Clear Session object
+    def tear_down(self):
         db.session.remove()
-
-        # Delete tables
         db.drop_all()
 
     def test_add_user(self):
-        
         u = User(username='Andrew', email='andrew@email.com')
-        
         db.session.add(u)
         db.session.commit()
-
         users = User.query.all()
 
         self.assertTrue(len(users) > 0)
