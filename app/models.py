17a74815446bd4ebb47a6c17505ba50a979abe9d
adkwalters <adkwalters@gmail.com>
2021-10-13 10:43:06 +0100

    Pass test: hash user password and check user password hash

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
@@ -5,3 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True)
+    password_hash = db.Column(db.String, unique=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
