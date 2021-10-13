d5b898c59af7744191ca100446f4f9939b6e6c47
adkwalters <adkwalters@gmail.com>
2021-10-13 14:31:58 +0100

    Add test: log user in and out

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+from flask_login import current_user, login_user, logout_user
 from app import app, db
 from app.models import User
 
@@ -18,14 +19,33 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
         users = User.query.all()
 
-        self.assertTrue(len(users) > 0)
+        self.assertTrue(len(users) > 0)    # User in database
 
     def test_hash_user_password(self):
         u = User(username='Andrew', email='andrew@email.com')
         u.set_password('password')
 
-        self.assertTrue(u.check_password('password'))
-        self.assertFalse(u.check_password('not the password'))
+        self.assertTrue(u.check_password('password'))    # Correct password
+        self.assertFalse(u.check_password('not the password'))    # Incorrect password
+
+    def test_log_user_in_and_out(self):
+        u = User(username='Andrew', email='andrew@email.com')
+        u.set_password('password')
+        db.session.add(u)
+        db.session.commit()
+        user = User.query.filter_by(username='Andrew').first()
+
+        self.assertFalse(current_user)    # No user logged in
+
+        with app.test_request_context():
+            login_user(user)
+
+        self.assertTrue(current_user)    # User logged in
+
+        with app.test_request_context():
+            logout_user(user)
+
+        self.assertFalse(current_user)    # No user logged in
 
 
 if __name__ == '__main__':
