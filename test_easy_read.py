b493aa3fc910105621a10da9eacff0cb006f58c3
adkwalters <adkwalters@gmail.com>
2021-10-15 17:59:12 +0100

    Integrate old tests into new test infrastructure
    - delete old tests file

--- test_easy_read.py
+++ test_easy_read.py
@@ -1,6 +1,8 @@
 import unittest
 from flask import current_app
-from app import create_app
+from flask_login import current_user, login_user, logout_user
+from app import create_app, db
+from app.models import User
 
 
 class TestWebApp(unittest.TestCase):
@@ -26,3 +28,32 @@ class TestWebApp(unittest.TestCase):
         assert response.request.path == '/login'
 
     
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+        self.client = self.app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.drop_all()
+        self.appctx.pop()
+        self.app = None
+        self.appctx = None
+        self.client = None
+
+    def test_add_user(self):
+        u = User(username='Andrew', email='andrew@email.com')
+        db.session.add(u)
+        db.session.commit()
+        users = User.query.all()
+        self.assertTrue(len(users) > 0)
+
+    def test_hash_user_password(self):
+        u = User(username='Andrew', email='andrew@email.com')
+        u.set_password('password')
+        self.assertTrue(u.check_password('password'))
+        self.assertFalse(u.check_password('not the password'))
+
+        
