2b84069e7e46c07a63241ed9bd266a9dfefe83fd
adkwalters <adkwalters@gmail.com>
2021-10-15 17:50:47 +0100

    Add test client to allow injection of fake requests
    This is what the whole app refactoring was for. Now I can imitate a user's actions
    across the app by crafting requests. After I integrate the old tests, I will create
    a test to log a user in via the login route, not the login() function.

--- test_easy_read.py
+++ test_easy_read.py
@@ -8,12 +8,21 @@ class TestWebApp(unittest.TestCase):
         self.app = create_app()
         self.appctx = self.app.app_context()
         self.appctx.push()
+        self.client = self.app.test_client()
 
     def tearDown(self):
         self.appctx.pop()
         self.app = None
         self.appctx = None
+        self.client = None
 
     def test_app(self):
         assert self.app is not None
         assert current_app == self.app
+
+    def test_home_page_redirect(self):
+        response = self.client.get('/author-articles', follow_redirects=True)
+        assert response.status_code == 200
+        assert response.request.path == '/login'
+
+    
