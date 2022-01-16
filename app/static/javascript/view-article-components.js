c0002fbba7f8352aaae6dbe79a73ff13f6074f3b
adkwalters <adkwalters@gmail.com>
2022-01-16 11:10:53 +0000

    Feat: build out article view and preview
    Render an article's main body content using a custom web component,
    into which encapsulate the styles and methods related to summary
    paragraphs.
    
    To control the display of selected reading levels, push a paragraph's
    summaries into an array that is set as a property of the custom
    webcomponent. Upon selection to increase or decrease the reading level,
    iterate through this array and display the next level. Should the
    beginning or end of the array be reached, remove the control to further
    decrease or increase the level, respectively.
    
    Add additional functionality to allow the user to set a default reading
    level, change the font size, and print or save the article with or
    without images.

---
+++ app/static/javascript/view-article-components.js
@@ -0,0 +1,180 @@
+// <summary-paragraph>
+class SummaryParagraph extends HTMLElement {
+    constructor() {
+        super();
+
+        // Create object array to hold summaries
+        this.summaries = [];
+
+        // Set the reading level
+        this.setLevel = (difficulty) => {
+            // Hide all summaries
+            for (let summary of this.summaries) {
+                summary.classList.remove("cursor");
+                summary.classList.add("no-display");
+            }
+            // Display highest level
+            if (difficulty == "hardest") {
+                this.summaries.at(-1).classList.replace("no-display", "cursor");            
+            }
+            // Display lowest level
+            else if (difficulty == "easiest") {
+                this.summaries.at(0).classList.replace("no-display", "cursor");          
+            }
+        }
+
+        // Increase reading level
+        this.increaseLevel = () => {
+            let cursor = this.summaries.indexOf(this.querySelector(".cursor"));
+            let current = this.summaries[cursor];
+            let next = this.summaries[cursor + 1];
+            if (next) {
+                // Hide current level and display next level
+                current.classList.replace("cursor", "no-display");
+                next.classList.replace("no-display", "cursor");
+            }
+        }
+
+        // Decrease reading level
+        this.decreaseLevel = () => {
+            let cursor = this.summaries.indexOf(this.querySelector(".cursor"));
+            let current = this.summaries[cursor];
+            let previous = this.summaries[cursor - 1];
+            if (previous) {
+                // Hide current level and display previous level
+                current.classList.replace("cursor", "no-display");
+                previous.classList.replace("no-display", "cursor");  
+            }
+        }
+
+        // Display arrows controls to inc/decrease level
+        this.displayArrows = () => {
+            // Get arrows
+            let incArrow = this.shadowRoot.querySelector(".next");
+            let decArrow = this.shadowRoot.querySelector(".prev");
+            // If highest level, remove increase arrow
+            if (this.summaries.at(-1).classList.contains("cursor")) {
+                incArrow.classList.add("no-display");
+            } else {
+                incArrow.classList.remove("no-display");
+            }  
+            // If lowest level, remove decrease arrow
+            if (this.summaries.at(0).classList.contains("cursor")) {
+                decArrow.classList.add("no-display");
+            } else {
+                decArrow.classList.remove("no-display");
+            }  
+        }
+    }
+
+    connectedCallback() {
+
+        // Attach shadow DOM
+        const shadow = this.attachShadow({mode: "open"});
+
+        // Add styling
+        const style = document.createElement("style");
+        style.textContent = `
+            ::slotted([slot=slot-image]) {
+                width: 100%;
+            }            
+            ::slotted([slot=slot-summary]) {
+                margin: 1rem;
+                background: #EEE;
+                border-radius: 10px;
+            }
+            div.summary-div {
+                position: relative;
+            }
+            .prev,
+            .next {
+                position: absolute;
+                transform: translateY(-50%);
+                top: 50%;
+                background: none;
+                border: none;
+                font-size: 2rem;
+                color: #888;
+            }
+            .prev {
+                left: -1rem;
+            }
+            .next {
+                right: -1rem;
+            }
+            .prev:hover,
+            .next:hover {
+                color: black;
+            }
+            .no-display {
+                display: none;
+            }
+
+            @media (min-width: 42rem) {
+                .prev {
+                    left: -2rem;
+                }
+                .next {
+                    right: -2rem;
+                }
+            }
+
+            @media print {
+                .prev,
+                .next {
+                    display: none;
+                }
+            }
+            `;
+
+        // Prepare elements
+        const div = document.createElement("div");
+        div.setAttribute("class", "summary-div");
+
+        const imageSlot = document.createElement("slot");
+        imageSlot.setAttribute("name", "slot-image");
+
+        const headerSlot = document.createElement("slot");
+        headerSlot.setAttribute("name", "slot-header");
+
+        const summarySlot = document.createElement("slot");
+        summarySlot.setAttribute("name", "slot-summary");
+
+        const incButton = document.createElement("button");
+        incButton.setAttribute("class", "next");
+        incButton.innerHTML = "&#10095;";
+
+        const decButton = document.createElement("button");
+        decButton.setAttribute("class", "prev");
+        decButton.innerHTML = "&#10094;";        
+
+        // Append elements to shadow DOM
+        shadow.appendChild(style);
+        shadow.appendChild(imageSlot);
+        shadow.appendChild(headerSlot);
+        shadow.appendChild(div);
+            div.appendChild(summarySlot);
+            div.appendChild(decButton);
+            div.appendChild(incButton);
+
+        // Append slotted summaries to object array
+        summarySlot.addEventListener("slotchange", () => {
+            this.summaries = summarySlot.assignedNodes();
+        })
+
+        // Increase reading level and configure arrow controls
+        incButton.addEventListener("click", () => {
+            this.increaseLevel();
+            this.displayArrows();
+        });
+
+        // Decrease reading level and configure arrow controls
+        decButton.addEventListener("click", () => {
+            this.decreaseLevel();
+            this.displayArrows();
+        });
+    }
+}
+customElements.define("summary-paragraph", SummaryParagraph);
+
+
---
+++ app/static/javascript/view-article-control.js
@@ -0,0 +1,83 @@
+// || Article Font Size
+
+// Get font-size selection buttons
+const smallFont = document.getElementById("font-size-small");
+const mediumFont = document.getElementById("font-size-medium");
+const largeFont = document.getElementById("font-size-large");
+
+// Change article font size to selected size
+const changeFontSize = (size) => {
+    // Get paragraphs
+    let paragraphs = document.querySelectorAll("summary-paragraph");
+    for (let paragraph of paragraphs) {
+        // Get headers and summaries
+        let summaries = paragraph.querySelectorAll(
+            "[slot='slot-header'], [slot='slot-summary']");
+        for (let summary of summaries) {
+            // Replace font size with selected size
+            summary.classList.remove("small-font", "medium-font", "large-font");
+            summary.classList.add(size);
+        } 
+    }
+}
+
+// Attach function to buttons
+smallFont.addEventListener("click", () => { changeFontSize("small-font"); });
+mediumFont.addEventListener("click", () => { changeFontSize("medium-font"); });
+largeFont.addEventListener("click", () => { changeFontSize("large-font"); });
+
+
+// || Article Reading Level
+
+// Get reading level selection
+const easiest = document.getElementById("easiest-level");
+const hardest = document.getElementById("hardest-level");
+
+// Change article reading level to selected level
+const changeLevel = (difficulty) => {
+    // Get all summaries
+    let paragraphs = document.querySelectorAll("summary-paragraph");
+    for (let paragraph of paragraphs) {
+        // Change level and configure arrow controls
+        paragraph.setLevel(difficulty);
+        paragraph.displayArrows();
+    }
+}
+
+// Attach function to buttons
+easiest.addEventListener("click", () => { changeLevel("easiest"); });
+hardest.addEventListener("click", () => { changeLevel("hardest"); });
+
+// Set default level
+changeLevel("easiest");
+
+
+// || Save and Print Article
+
+// Get save and print buttons
+const printWithImages = document.getElementById("with-images");
+const printWithoutImages = document.getElementById("without-images");
+
+// Get document header
+const head = document.querySelector("head");
+
+// Create stylesheet with rule to remove images from print
+const stylesheet = document.createElement("style");
+stylesheet.innerHTML = "@media print { img { display: none; } }";
+
+// Print article with images
+printWithImages.addEventListener("click", () => {
+    if (head.contains(stylesheet)) {
+        head.removeChild(stylesheet);
+    }
+    print()
+})
+
+// Print article without images
+printWithoutImages.addEventListener("click", () => {
+    if (!head.contains(stylesheet)) {
+        head.appendChild(stylesheet);
+    }
+    print()
+    
+})
