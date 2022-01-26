6acab46e0113587d0eb7b2d4d07e54c652ecded1
adkwalters <adkwalters@gmail.com>
2022-01-26 13:45:01 +0000

    Add: contact page for users to contact admin
    Allow users to send messages to admin with a dedicated contact page.
    
    Add a new form to validate user message input. Render the form in a
    new view available to the general public.
    
    Link the contact page from the newly created error pages.

--- app/main/forms.py
+++ app/main/forms.py
@@ -52,3 +52,12 @@ class EmailForm(FlaskForm):
     article_id          = IntegerField('Id')
     user_email          = StringField('Email', validators=[DataRequired(), Email()])
 
+
+class ContactForm(FlaskForm):
+    """A form to collect and validate an email message"""
+    name                = StringField('Name', validators=[DataRequired()])
+    email               = StringField('Email', validators=[DataRequired(), Email()])
+    subject             = StringField('Subject', validators=[DataRequired()])
+    message             = TextAreaField('Message', validators=[DataRequired()])
+    submit              = SubmitField('Send')
+
--- app/main/routes.py
+++ app/main/routes.py
@@ -86,7 +86,7 @@ from werkzeug.utils import secure_filename
 
 from app import db, mail
 from app.main import bp
-from app.main.forms import ArticleForm, ImageForm, EmailForm
+from app.main.forms import ArticleForm, ImageForm, EmailForm, ContactForm
 from app.models import Article, Source, Category, Image, Paragraph, Summary, User, Publisher, PublishingNote
 from config import basedir
 
@@ -1470,7 +1470,7 @@ def filter_articles():
         .outerjoin(PublishingNote, PublishingNote.published_article_id == Article.id) \
         .filter(Article.status == 'pub_live').all()
 
-    # Render index page
+    # Render articles filter page
     return render_template('articles.html', 
         categories=categories,
         dates=dates,
@@ -1479,9 +1479,39 @@ def filter_articles():
 
 @bp.route('/about')
 def about():
-    """Display an article about Easy Read"""       
-    # Render index page
+    """Display an article about Easy Read"""
+
+    # Render about page
     return render_template('about.html')
+
+
+@bp.route('/contact', methods=['GET', 'POST'])
+def contact():
+    """Display a form to contact Easy Read"""
+
+    # Get contact form
+    form = ContactForm()
+
+    if form.validate_on_submit():
+    
+        # Compose and send email to admin
+        msg = Message(
+            subject = form.subject.data,
+            sender = form.name.data,
+            reply_to = form.email.data,
+            recipients = [current_app.config['ADMIN'][0]],
+            bcc = [current_app.config['ADMIN'][1]], # me
+            body = f'{form.name.data} has sent the following message: \n\n { form.message.data}')
+        mail.send(msg)
+
+        # Report message sent
+        flash('Message successfully sent. Please await a response.', 'success')
+
+        # Refresh page
+        return redirect(url_for('main.contact'))
+
+    # Render contact page
+    return render_template('contact.html', form=form)
 
 
 @bp.route('/<int:id>/')
