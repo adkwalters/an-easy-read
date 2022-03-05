9da0938776b61f73b84ebea11907f36082592e05
adkwalters <adkwalters@gmail.com>
2022-03-05 12:30:45 +0000

    Add: link to database schema design from models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+# Database schema: 
+# https://an-easy-read.s3.eu-west-2.amazonaws.com/database_schema.pdf
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app
 from flask_login import UserMixin
